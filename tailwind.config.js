4df69a7a99637ac7865964964eaf052460856df9
DharsDev <121114000+DharshanPalani@users.noreply.github.com>
2024-12-18 21:31:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "./src/**/*.{js,ts,jsx,tsx}", // Update based on your file structure
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
